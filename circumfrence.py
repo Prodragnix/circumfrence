ce654c8662896409c3531ed25009067ae032f064
Prodragnix <connect.neilkumar@gmail.com>
2024-12-11 16:29:22 +0000

    Update circumfrence.py

--- circumfrence.py
+++ circumfrence.py
@@ -1,15 +1,23 @@
 import math
 import turtle
-r=int(input('Please enter the radius: '))
-ans=math.pi*r*2
-print(ans,'is the cicumfrence of the radius of:',r)
-pen=turtle.Turtle()
-screen=turtle.Screen()
-pen.circle(r)
-pen.penup()
-pen.goto(0,-100)
-pen.pendown()
-pen.write(str(ans)+' is your circumfrence.', font=("Comic Sans MS",
+def function():
+    r=int(input('\nPlease enter the radius: '))
+    ans=math.pi*r*2
+    print(ans,'is the cicumfrence of the radius of:',r)
+    pen=turtle.Turtle()
+    screen=turtle.Screen()
+    pen.circle(r)
+    pen.penup()
+    pen.goto(0,-100)
+    pen.pendown()
+    pen.write(str(ans)+' is your circumfrence.', font=("Comic Sans MS",
                                     20, "normal"))
-screen.mainloop()
+    screen.mainloop()
+    user=input('\nDo you want to use this again? y or n: ')
+    if user=='y':
+        function()
+    else:
+        print('\nThanks you. See you soon!\n\n')
+        
+function()
 
